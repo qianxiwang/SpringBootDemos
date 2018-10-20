352f20995a0a5ea524ce6f488dde7fd52554f0c6
mac <2734756709@qq.com>
2018-10-21 01:45:11 +0800

    初始化一个SpringBoot的新工程

---
+++ src/main/java/com/demos/springbootdemos/SpringbootdemosApplication.java
@@ -0,0 +1,12 @@
+package com.demos.springbootdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemosApplication.class, args);
+    }
+}
---
+++ src/test/java/com/demos/springbootdemos/SpringbootdemosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demos.springbootdemos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootdemosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
